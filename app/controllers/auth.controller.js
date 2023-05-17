32b0ef80519fa84ceaf9fecc8f274998b5871be9
mdkarim-codeclouds <md.karim@codeclouds.com>
2023-05-17 18:25:39 +0530

    token added in response

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -105,6 +105,7 @@ exports.signin = (req, res) => {
             username: user.username,
             email: user.email,
             roles: authorities,
+            token: token,
         });
     });
 };
