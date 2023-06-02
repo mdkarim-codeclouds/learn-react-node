6f19f5cc2adca870e8a4a8a8b17ff5a14d066d0f
mdkarim-codeclouds <md.karim@codeclouds.com>
2023-06-02 20:18:10 +0530

    delete

--- backend/app/config/auth.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    secret: "learn-react-node-secret-key"
-};
--- backend/app/config/db.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    PREHOST: "mongodb+srv",
-    HOST: "sandbox.hhjm5.mongodb.net",
-    PORT: 27017,
-    DB: "node-mongodb",
-    USERNAME: "md-karim-cc",
-    PASSWORD: "Miron786"
-};
